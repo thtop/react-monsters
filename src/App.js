6a3b764f72b395e77fb31eda5c897f7a5009ef5a
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-07 02:53:01 +0700

    Finished item 32 SearchField state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
-  state = { monsters: []}
+  state = { 
+    monsters: [],
+    searchField: ''
+  };
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -14,6 +17,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input 
+          type='text' 
+          placeholder='search monsters'
+          onChange={e => this.setState({ searchField: e.target.value})}
+        />
         <CardList monsters={this.state.monsters} />>        
       </div>
     )
