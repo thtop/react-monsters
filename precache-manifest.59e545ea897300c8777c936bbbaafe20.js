20b9e1be7b5ac564beb760c4f01a9e1f2fe80d62
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-08-08 04:08:45 +0700

    Updates

---
+++ precache-manifest.59e545ea897300c8777c936bbbaafe20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd1876ea34a53ad69bd351f49e8d6ea",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "8a8b06ffaff407bf9be1",
+    "url": "/react-monsters/static/css/main.1b2e7d3a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8a8b06ffaff407bf9be1",
+    "url": "/react-monsters/static/js/main.cd774f56.chunk.js"
+  },
+  {
+    "revision": "1c8ad7999933b0847d83",
+    "url": "/react-monsters/static/js/runtime~main.511ef2b8.js"
+  }
+]);
